f8c124bbf9a8f8fb3af22b3f2eab79e351be454b
RootV <39199575+rootv99@users.noreply.github.com>
2021-07-13 10:59:12 -0400

    Uploaded main.java file

---
+++ Main.java
@@ -0,0 +1,10 @@
+package com.pyramid_academy;
+
+public class Main {
+
+    public static void main(String[] args) {
+        DragonCave.chooseTheCave();
+        GuessTheNumber.guessNums();
+        Hangman.game();
+    }
+}
